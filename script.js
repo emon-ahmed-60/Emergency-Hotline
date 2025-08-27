f9cdeedb2b6dcefdc42a3ad53b5b1aaf6dbe3647
Emon Ahmed <emon0860@gmail.com>
2025-08-27 20:25:58 +0600

    add Call Buttons Functionalities and Call History Section Functionalities

--- script.js
+++ script.js
@@ -8,3 +8,53 @@ for(const heartIcon of heartIcons){
     })
 }
 
+document.getElementById('cartContainer').addEventListener("click",(e)=>{
+    let coin = document.getElementById('call-coin').innerText;
+    if(coin < 20){
+        return alert("coin not enough");
+    }
+
+    if(e.target.className.includes('call-btn')){
+        coin -= 20;
+        const callBtn = e.target;
+        const serviceName = callBtn.parentNode.parentNode.children[1].innerText;
+       const serviceNumber = callBtn.parentNode.parentNode.children[3].innerText;
+       alert(serviceName + '\n' + serviceNumber);
+       const div = document.createElement('div');
+       div.innerHTML = `
+       <div class="example rounded-lg bg-[#fafafa] p-4 flex justify-between items-center gap-4 mb-2">
+                <div>
+                    <h1 class="font-semibold text-lg">${serviceName}</h1>
+                    <p class="text-lg text-[#5c5c5c]">${serviceNumber}</p>
+                </div>
+                <p class="text-[#111111] text-lg">${new Date().toLocaleTimeString()}</p>
+          </div>
+       `
+       document.getElementById('call-history').append(div);
+       
+       return document.getElementById('call-coin').innerText = coin;
+        
+    } else if(e.target.className.includes('fa-phone')){
+        coin -= 20;
+     const callBtn = e.target;
+        const serviceName = callBtn.parentNode.parentNode.parentNode.children[1].innerText;
+       const serviceNumber = callBtn.parentNode.parentNode.parentNode.children[3].innerText;
+        alert(serviceName + '\n' + serviceNumber);
+        const div = document.createElement('div');
+       div.innerHTML = `
+       <div class="example rounded-lg bg-[#fafafa] p-4 flex justify-between items-center gap-4 mb-2">
+                <div>
+                    <h1 class="font-semibold text-lg">${serviceName}</h1>
+                    <p class="text-lg text-[#5c5c5c]">${serviceNumber}</p>
+                </div>
+                <p class="text-[#111111] text-lg">${new Date().toLocaleTimeString()}</p>
+          </div>
+       `
+       document.getElementById('call-history').append(div);
+        return document.getElementById('call-coin').innerText = coin;
+    }
+})
+
+document.getElementById('clear-btn').addEventListener("click",()=>{
+    document.getElementById('call-history').innerHTML = '';
+})
